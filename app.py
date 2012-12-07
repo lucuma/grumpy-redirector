304cf152fdd5f5f983529d1ba9ad226d768ed463
Jay-P <juanpablo@lucumalabs.com>
2012-12-07 12:38:04 -0500

    whatever

--- app.py
+++ app.py
@@ -7,14 +7,20 @@ import requests
 DEFAULT_TARGET = 'http://localhost:8080'
 
 
-def app(environ, start_response):
-    request = Request(environ)
+def redirect_request(request):
     target = request.args.get('target', DEFAULT_TARGET)
     requests.post(target, data=request.form, files=request.files)
+
+
+def application(environ, start_response):
+    request = Request(environ)
+
+    redirect_request(request)
+
     response = Response('Redirector activated!', mimetype='text/plain')
     return response(environ, start_response)
 
 
 if __name__ == '__main__':
     from werkzeug.serving import run_simple
-    run_simple('localhost', 4000, app)
+    run_simple('localhost', 4000, application)
